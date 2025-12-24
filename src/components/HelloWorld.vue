8fb7c65cba001610d149647668b501d2b1754f80
mekane <4631242+mekane84@users.noreply.github.com>
2025-12-24 11:56:26 -0600

    improve visuals

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,6 +17,10 @@ h1 {
   font-family: 'Dancing Script', 'Brush Script MT', cursive;
   font-weight: 500;
   font-size: 4.2rem;
+  text-transform: none; /* Scripts usually look better without uppercase */
+  letter-spacing: normal;
+  color: #e0a800;
+  text-shadow: 2px 2px 4px rgba(0,0,0,0.8);
   position: relative;
   top: -10px;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,27 +4,29 @@
 
 <template>
   <main>
-    Tournaments:
-    <ul>
-      <li>
-        <router-link to="/2023usopen">2023 US Open</router-link>
-      </li>
-      <li>
-        <router-link to="/2024therack">2024 The Rack One Pocket Classic</router-link>
-      </li>
-      <li>
-        <router-link to="/2024bigtymeclassic">2024 Big Tyme Classic</router-link>
-      </li>
-      <li>
-        <router-link to="/2024/scottytownsendmemorial">2024 Scotty Townsend Memorail</router-link>
-      </li>
-      <li>
-        <router-link to="/2024/champions-challenge">2024 Champions Challenge</router-link>
-      </li>
-      <li>
-        <router-link to="/2024/buffalospc">2024 Buffalo&lsquo;s Pro Classic</router-link>
-      </li>
-    </ul>
+    <div class="container">
+      Tournaments:
+      <ul>
+        <li>
+          <router-link to="/2023usopen">2023 US Open</router-link>
+        </li>
+        <li>
+          <router-link to="/2024therack">2024 The Rack One Pocket Classic</router-link>
+        </li>
+        <li>
+          <router-link to="/2024bigtymeclassic">2024 Big Tyme Classic</router-link>
+        </li>
+        <li>
+          <router-link to="/2024/scottytownsendmemorial">2024 Scotty Townsend Memorail</router-link>
+        </li>
+        <li>
+          <router-link to="/2024/champions-challenge">2024 Champions Challenge</router-link>
+        </li>
+        <li>
+          <router-link to="/2024/buffalospc">2024 Buffalo&lsquo;s Pro Classic</router-link>
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
 
