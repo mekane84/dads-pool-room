7e22375d711c94c0db531f1c7e95b6063a42afa5
mekane <4631242+mekane84@users.noreply.github.com>
2025-12-24 12:02:28 -0600

    improve year sorting and display grouping

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,44 @@
 <script setup>
+import { computed } from 'vue';
 
+const tournaments = [
+  { name: "US Open", path: "/2025usopen", year: 2025 },
+  { name: "Buffalo's Pro Classic", path: "/2024/buffalospc", year: 2024 },
+  { name: "Champions Challenge", path: "/2024/champions-challenge", year: 2024 },
+  { name: "Scotty Townsend Memorial", path: "/2024/scottytownsendmemorial", year: 2024 },
+  { name: "Big Tyme Classic", path: "/2024bigtymeclassic", year: 2024 },
+  { name: "The Rack One Pocket Classic", path: "/2024therack", year: 2024 },
+  { name: "US Open", path: "/2023usopen", year: 2023 },
+];
+
+const groupedTournaments = computed(() => {
+  return tournaments.reduce((groups, tournament) => {
+    const year = tournament.year;
+    if (!groups[year]) {
+      groups[year] = [];
+    }
+    groups[year].push(tournament);
+    return groups;
+  }, {});
+});
+
+const sortedYears = computed(() => {
+  return Object.keys(groupedTournaments.value).sort((a, b) => b - a);
+});
 </script>
 
 <template>
   <main>
     <div class="container">
       Tournaments:
-      <ul>
-        <li>
-          <router-link to="/2024/buffalospc">2024 Buffalo&lsquo;s Pro Classic</router-link>
-        </li>
-        <li>
-          <router-link to="/2024/champions-challenge">2024 Champions Challenge</router-link>
-        </li>
-        <li>
-          <router-link to="/2024/scottytownsendmemorial">2024 Scotty Townsend Memorail</router-link>
-        </li>
-        <li>
-          <router-link to="/2024bigtymeclassic">2024 Big Tyme Classic</router-link>
-        </li>
-        <li>
-          <router-link to="/2024therack">2024 The Rack One Pocket Classic</router-link>
-        </li>
-        <li>
-          <router-link to="/2023usopen">2023 US Open</router-link>
-        </li>
-      </ul>
+      <div v-for="year in sortedYears" :key="year">
+        <h3>{{ year }}</h3>
+        <ul>
+          <li v-for="tournament in groupedTournaments[year]" :key="tournament.path">
+            <router-link :to="tournament.path">{{ tournament.name }}</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
   </main>
 </template>
