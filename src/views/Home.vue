3e25d0c2eaabed5cc4b7053f3d198b6b6feacb9b
mekane <4631242+mekane84@users.noreply.github.com>
2025-12-24 15:55:59 -0600

    add dates for tournaments

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,13 @@
 import { computed } from 'vue';
 
 const tournaments = [
-  { name: "US Open", path: "/2025/usopen", year: 2025 },
-  { name: "Buffalo's Pro Classic", path: "/2024/buffalospc", year: 2024 },
-  { name: "Champions Challenge", path: "/2024/champions-challenge", year: 2024 },
-  { name: "Scotty Townsend Memorial", path: "/2024/scottytownsendmemorial", year: 2024 },
-  { name: "Big Tyme Classic", path: "/2024bigtymeclassic", year: 2024 },
-  { name: "The Rack One Pocket Classic", path: "/2024therack", year: 2024 },
-  { name: "US Open", path: "/2023usopen", year: 2023 },
+  { name: "US Open", path: "/2025/usopen", year: 2025, date: "Aug 18-23" },
+  { name: "Buffalo's Pro Classic", path: "/2024/buffalospc", year: 2024, date: "Nov 25-29" },
+  { name: "Champions Challenge", path: "/2024/champions-challenge", year: 2024, date: "Oct 15-17" },
+  { name: "Scotty Townsend Memorial", path: "/2024/scottytownsendmemorial", year: 2024, date: "Aug 29 - Sep 1" },
+  { name: "Big Tyme Classic", path: "/2024bigtymeclassic", year: 2024, date: "Jun 12-16" },
+  { name: "The Rack One Pocket Classic", path: "/2024therack", year: 2024, date: "Mar 11-13" },
+  { name: "US Open", path: "/2023usopen", year: 2023, date: "Oct 23-28" },
 ];
 
 const groupedTournaments = computed(() => {
@@ -36,6 +36,7 @@ const sortedYears = computed(() => {
         <ul>
           <li v-for="tournament in groupedTournaments[year]" :key="tournament.path">
             <router-link :to="tournament.path">{{ tournament.name }}</router-link>
+            <span v-if="tournament.date" class="tournament-date"> &ndash; {{ tournament.date }}</span>
           </li>
         </ul>
       </div>
@@ -44,4 +45,10 @@ const sortedYears = computed(() => {
 </template>
 
 <style scoped>
+.tournament-date {
+  font-family: "Georgia", serif;
+  font-style: italic;
+  font-size: 0.9em;
+  color: #888;
+}
 </style>
