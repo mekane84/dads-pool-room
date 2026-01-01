3840434e835109c66b80a6b3cc4a26b00393c547
mekane <4631242+mekane84@users.noreply.github.com>
2026-01-01 15:10:08 -0600

    hanoi open 2025 more tournament info

--- src/views/2025/hanoiopen/HanoiOpenPoolChampionship2025.vue
+++ src/views/2025/hanoiopen/HanoiOpenPoolChampionship2025.vue
@@ -15,7 +15,13 @@ import IconFinals from "@/components/icons/IconFinals.vue";
         </template>
         <template #heading>Tournament Info</template>
         <p>
-          The Hanoi Open Pool Championship took place in Hanoi, Vietnam, featuring the world's best 9-ball players.
+          The Hanoi Open returns for its third edition from 7–12 October, once again lighting up Vietnam’s capital
+          with world-class nineball action.
+        </p>
+        <p>
+          With the prize fund rising to $200,000 and a boosted $40,000 top prize, the stakes have never been higher.
+          Defending champion Johann Chua will aim to retain his crown after his thrilling win over Ko Pin Yi last year.
+          But with the world’s best on the hunt, can Chua go back-to-back—or will a new king emerge in Hanoi?
         </p>
       </WelcomeItem>
 
